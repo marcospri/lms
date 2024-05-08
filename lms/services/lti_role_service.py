64363b2788f587cd6490bee10fbbb786a856c4d0
Marcos Prieto <marcospri@gmail.com>
2024-05-08 15:36:34 +0200

    Disable pylint's no-member and not-callable
    They mostly produce false positives and this is better covered by mypy

--- lms/services/lti_role_service.py
+++ lms/services/lti_role_service.py
@@ -42,7 +42,7 @@ class LTIRoleService:
         self, ai: ApplicationInstance, roles: list[LTIRole]
     ) -> list[Role]:
         self._db.flush()  # Make sure roles have IDs
-        # pylint:disable=no-member
+
         overrides = self._db.execute(  # type: ignore
             select(
                 LTIRole.value.label("value"),
